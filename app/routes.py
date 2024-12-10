9c5736c1ad962d87cc102a8d851211f7521a4f92
Estebyte <estebanloaizamora@gmail.com>
2024-12-10 13:11:43 +0000

    POST to the db with a form

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,32 @@ from models import Person
 
 def register_routes(app, db):
 
-    @app.route("/")
+    @app.route("/", methods=["GET", "POST"])
     def index():
-        #Query all in person table
-        people = Person.query.all()
-        context = {
-            "people" : people
-        }
-        return render_template("index.html", **context)
+        if request.method == "GET":
+            #Query all in person table
+            people = Person.query.all()
+            context = {
+                "people" : people
+            }
+            return render_template("index.html", **context)
+        
+        elif request.method == "POST":
+            #Get the values from the form
+            name = request.form.get("name")
+            age = int(request.form.get("age"))
+            job = request.form.get("job")
+
+            #Create a person object with the values of the form
+            person = Person(name=name, age=age, job=job)
+
+            #Add the person to the db and commit the changes
+            db.session.add(person)
+            db.session.commit()
+
+            #Query all in person table
+            people = Person.query.all()
+            context = {
+                "people" : people
+            }
+            return render_template("index.html", **context)
