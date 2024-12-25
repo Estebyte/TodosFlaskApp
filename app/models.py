a30bb6f8eac4993aff85ae3103103b60f41992af
Estebyte <estebanloaizamora@gmail.com>
2024-12-25 13:08:22 +0000

    Added 1:N relationships

--- app/models.py
+++ app/models.py
@@ -7,8 +7,20 @@ class Person(db.Model):
 
     p_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
-    age = db.Column(db.Integer)
+    age = db.Column(db.Integer, nullable=False)
     job = db.Column(db.Text)
+    todos = db.relationship("Todos", backref="responsible", lazy=True)
 
     def __repr__(self):
         return f"Person with name {self.name} and age {self.age}"
+    
+# 1:N Relationship
+class Todos(db.Model):
+    __tablename__ = "todos"
+
+    t_id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.Text, nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey("people.p_id"), nullable=False)
+
+    def __repr__(self):
+        return f"To do: {self.todo}"
